683b6d48fca6230835c31535cdb09eb0d8aa5823
anish <anishbasnetworld@gmail.com>
2021-10-19 15:07:50 +0545

    Final version Install of twitter covid 19.

--- examples/tf-idf/BoW_frequency_optimizer_example.py
+++ examples/tf-idf/BoW_frequency_optimizer_example.py
@@ -18,7 +18,7 @@ from tweeter_covid19.utils import get_all_directory, read_data_from_file, list_f
 N_SETS = 10
 if __name__ == '__main__':
     for fold in range(N_SETS):
-        BoW_path = os.path.join('data', 'processing', 'tf-idf', 'BoW', 'set_' + str(fold + 1),
+        BoW_path = os.path.join('data', 'tfidf_processing', 'tf-idf', 'BoW', 'set_' + str(fold + 1),
                                 'bag_of_word.pkl')
 
         additional_stop_word_path = os.path.join('resources', 'dictionary', 'additional_stop_words.txt')
@@ -27,7 +27,7 @@ if __name__ == '__main__':
 
         test_path = os.path.join('data', 'fold_train_test_dataset_vectors', 'set_' + str(fold + 1), 'test.csv')
 
-        write_path = os.path.join('data', 'processing', 'tfidf_vector', 'set_' + str(fold + 1))
+        write_path = os.path.join('data', 'processing', 'tfidf_vector_test', 'set_' + str(fold + 1))
         mkdir(write_path)
 
         BoW = read_pickle_data(BoW_path)
@@ -40,21 +40,37 @@ if __name__ == '__main__':
 
         # first preprocess IDF count - single vector of 1 x 100
         print('Processing ')
-        tf_vector_train = np.zeros(shape=(train_df['Tweet'].shape[0], 100), dtype=float)
-        tf_vector_test = np.zeros(shape=(test_df['Tweet'].shape[0], 100), dtype=float)
+        train_Y = np.array([])
+        test_Y = np.array([])
+
+        # exit(0)
+        # tf_vector_train = np.zeros(shape=(train_df['Tweet'].shape[0], 100), dtype=float)
+        # tf_vector_test = np.zeros(shape=(test_df['Tweet'].shape[0], 100), dtype=float)
+
+        tf_vector_train = []
+        tf_vector_test = []
 
         idf_vector = np.zeros(shape=(100,), dtype=int)
         for t_index, tweet in enumerate(train_df['Tweet']):
-            for f_index, (bow_word, freq) in enumerate(filtered_BoW):
-                found_items = re.findall(bow_word, tweet)
-                tf_vector_train[t_index][f_index] = len(found_items) / len(tweet.split(' '))
-                if len(found_items) != 0:
-                    idf_vector[f_index] += 1
+            if len(tweet.split(' ')) > 2:
+                train_Y = np.append(train_Y, train_df['Label'][t_index])
+                vec = np.zeros(shape=(100,), dtype=float)
+                for f_index, (bow_word, freq) in enumerate(filtered_BoW):
+                    found_items = re.findall(bow_word, tweet)
+                    vec[f_index] = len(found_items) / len(tweet.split(' '))
+                    if len(found_items) != 0:
+                        idf_vector[f_index] += 1
+                tf_vector_train.append(vec)
+            # exit(0)
 
         for t_index, tweet in enumerate(test_df['Tweet']):
-            for f_index, (bow_word, freq) in enumerate(filtered_BoW):
-                found_items = re.findall(bow_word, tweet)
-                tf_vector_test[t_index][f_index] = len(found_items) / len(tweet.split(' '))
+            if len(tweet.split(' ')) > 2:
+                test_Y = np.append(test_Y, test_df['Label'][t_index])
+                vec = np.zeros(shape=(100,), dtype=float)
+                for f_index, (bow_word, freq) in enumerate(filtered_BoW):
+                    found_items = re.findall(bow_word, tweet)
+                    vec[f_index] = len(found_items) / len(tweet.split(' '))
+                tf_vector_test.append(vec)
 
         idf_vector = train_df.shape[0] / idf_vector
 
@@ -62,10 +78,9 @@ if __name__ == '__main__':
         idf_vector = np.log10(idf_vector + 1)
 
         train_X = tf_vector_train * idf_vector
-        train_Y = train_df['Label']
 
         test_X = tf_vector_test * idf_vector
-        test_Y = test_df['Label']
+
 
         write_pickle_data(os.path.join(write_path, 'train_x.pkl'), train_X)
         write_pickle_data(os.path.join(write_path, 'train_y.pkl'), train_Y)
